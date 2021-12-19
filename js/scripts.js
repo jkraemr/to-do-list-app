df7913050001808b3f4a7f07a537445ed44fdd75
Johannes Krämer <johannes.kraemer@hotmail.com>
2021-12-19 22:44:49 +0100

    Add jQuery for list item management functionality

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+function newItem() {
+
+  // Add new item to list via text input
+  let li = $('<li>');
+  let inputValue = $('#input').val();
+  li.append(inputValue);
+
+  if (inputValue === '') {
+    alert('Please enter some text');
+  } else {
+    $('#list').append(li);
+  }
+
+  // Cross out item from list via double-click
+  li.on('dblclick', function() {
+    li.toggleClass('strike');
+  });
+
+  // Remove item from list via button deleteButton with X
+  let deleteButton = $('<deleteButton>');
+  deleteButton.append(document.createTextNode('X'));
+  li.append(deleteButton);
+  deleteButton.on('click', function() {
+    li.addClass('delete');
+  });
+}
+
+// Reorder items in list via click & drag up/down
+$('#list').sortable();
